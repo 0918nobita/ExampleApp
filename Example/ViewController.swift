5123252dabb9ce28d312281741057d8483307cc8
0918nobita <nobita.0918@gmail.com>
2019-04-16 17:41:02 +0900

    :art: Improve readability

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -1,20 +1,8 @@
-//
-//  ViewController.swift
-//  Example
-//
-//  Created by 松本幸大 on 2019/04/15.
-//  Copyright © 2019 Kodai Matsumoto. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
